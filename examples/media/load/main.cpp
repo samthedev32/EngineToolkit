ab108434a728fede82b8573e4ac9ba25de21a2de
SamTheDev <samthedev32@gmail.com>
2023-10-10 07:40:09 +0200

    stuff

--- examples/media/load/main.cpp
+++ examples/media/load/main.cpp
@@ -6,73 +6,74 @@
 using namespace EngineToolkit;
 
 int main() {
-  // Image img = Image::load("../../../../examples/image/test.png");
-  Image img("../../../../examples/media/test.bmp");
+    // Image img = Image::load("../../../../examples/image/test.png");
+    Image img("../../../../examples/media/test.bmp");
 
-  for (int i = 0; i < img.size->w / 2; i++)
-    for (int j = 0; j < img.size->h / 2; j++) {
-      img(i, j)[0] += 128;
-      img(i, j)[1] += 128;
-      img(i, j)[2] += 128;
+    for (int i = 0; i < img.size->width / 2; i++)
+        for (int j = 0; j < img.size->height / 2; j++) {
+            img(i, j)[0] += 128;
+            img(i, j)[1] += 128;
+            img(i, j)[2] += 128;
+        }
+
+    // Initialize GLFW
+    if (!glfwInit()) {
+        return -1;
     }
 
-  // Initialize GLFW
-  if (!glfwInit()) {
-    return -1;
-  }
+    // Create tex windowed mode window and its OpenGL context
+    GLFWwindow *window =
+        glfwCreateWindow(600, 600, "Image Loading", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
 
-  // Create tex windowed mode window and its OpenGL context
-  GLFWwindow *window = glfwCreateWindow(600, 600, "Image Loading", NULL, NULL);
-  if (!window) {
-    glfwTerminate();
-    return -1;
-  }
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
 
-  // Make the window's context current
-  glfwMakeContextCurrent(window);
-
-  GLuint tex;
-  glGenTextures(1, &tex);
-  glBindTexture(GL_TEXTURE_2D, tex);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size->x, img.size->y, 0, GL_RGB,
-               GL_UNSIGNED_BYTE, img.data);
-
-  // Set texture parameters
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-  // Enable texture mapping
-  glEnable(GL_TEXTURE_2D);
-
-  // Main rendering loop
-  while (!glfwWindowShouldClose(window)) {
-    // Clear the buffer
-    glClear(GL_COLOR_BUFFER_BIT);
-
+    GLuint tex;
+    glGenTextures(1, &tex);
     glBindTexture(GL_TEXTURE_2D, tex);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size->x, img.size->y, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, img.data);
 
-    // Render tex quad with the texture
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f);
-    glVertex2f(-0.5f, -0.5f);
-    glTexCoord2f(1.0f, 1.0f);
-    glVertex2f(0.5f, -0.5f);
-    glTexCoord2f(1.0f, 0.0f);
-    glVertex2f(0.5f, 0.5f);
-    glTexCoord2f(0.0f, 0.0f);
-    glVertex2f(-0.5f, 0.5f);
-    glEnd();
+    // Set texture parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    // Swap front and back buffers
-    glfwSwapBuffers(window);
+    // Enable texture mapping
+    glEnable(GL_TEXTURE_2D);
 
-    // Poll for and process events
-    glfwPollEvents();
-  }
+    // Main rendering loop
+    while (!glfwWindowShouldClose(window)) {
+        // Clear the buffer
+        glClear(GL_COLOR_BUFFER_BIT);
 
-  // Cleanup
-  glDeleteTextures(1, &tex);
-  glfwTerminate();
+        glBindTexture(GL_TEXTURE_2D, tex);
 
-  return 0;
+        // Render tex quad with the texture
+        glBegin(GL_QUADS);
+        glTexCoord2f(0.0f, 1.0f);
+        glVertex2f(-0.5f, -0.5f);
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex2f(0.5f, -0.5f);
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex2f(0.5f, 0.5f);
+        glTexCoord2f(0.0f, 0.0f);
+        glVertex2f(-0.5f, 0.5f);
+        glEnd();
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    glDeleteTextures(1, &tex);
+    glfwTerminate();
+
+    return 0;
 }
